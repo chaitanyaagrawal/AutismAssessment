fb324dac6cacea7930e59a4feb3401bbfe123897
Chaitanya Agrawal <chaitanya.agrawal.13@ucl.ac.uk>
2014-09-13 23:31:44 +0100

    first commit

---
+++ AutismAssessment/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  AutismAssessment
+//
+//  Created by Chaitanya Agrawal on 12/09/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController{
+    IBOutlet UIScrollView *scroller;
+}
+@end
---
+++ AutismAssessment/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  AutismAssessment
+//
+//  Created by Chaitanya Agrawal on 12/09/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    [scroller setScrollEnabled:YES];
+    [scroller setContentSize:CGSizeMake(320, 1000)];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
