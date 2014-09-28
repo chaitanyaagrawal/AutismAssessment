ffe4add460a0ed688a14ed2ef0e36ac9d9033f8b
Chaitanya Agrawal <chaitanya.agrawal.13@ucl.ac.uk>
2014-09-28 18:38:04 +0100

    update

--- AutismAssessment/MainViewController.m
+++ AutismAssessment/MainViewController.m
@@ -31,7 +31,7 @@
     int statusBarHeight = [UIApplication sharedApplication].statusBarFrame.size.height;
     menuDrawerWidth = self.view.frame.size.width * 0.75;
     menuDrawerX = self.view.frame.origin.x  - menuDrawerWidth;
-    menuDrawer = [[UIView alloc] initWithFrame:CGRectMake(menuDrawerX, self.view.frame.origin.y , menuDrawerWidth, self.view.frame.size.height - statusBarHeight)]; //+ statusBarHeight to y axis co-ordinate for shifting slider menu below the status bar
+    menuDrawer = [[UIView alloc] initWithFrame:CGRectMake(menuDrawerX, self.view.frame.origin.y , menuDrawerWidth, self.view.frame.size.height)]; //+ statusBarHeight to y axis co-ordinate for shifting slider menu below the status bar, -statusBarHeight from height for not extending the slider menu to the bottom
     menuDrawer.backgroundColor = [UIColor whiteColor];
     
     recognizer_close = [[UISwipeGestureRecognizer alloc]initWithTarget:self action:@selector(handleSwipes:)];
