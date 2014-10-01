2ba13dfd6255b230d8f2e52e44aa5573609d6e7b
Chaitanya Agrawal <chaitanya.agrawal.13@ucl.ac.uk>
2014-10-01 03:43:16 +0100

    update

---
+++ AutismAssessment/RegisterController.h
@@ -0,0 +1,13 @@
+//
+//  RegisterControllerViewController.h
+//  AutismAssessment
+//
+//  Created by Chaitanya Agrawal on 01/10/14.
+//  Copyright (c) 2014 Chaitanya Agrawal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RegisterController : UIViewController
+
+@end
---
+++ AutismAssessment/RegisterController.m
@@ -0,0 +1,39 @@
+//
+//  RegisterControllerViewController.m
+//  AutismAssessment
+//
+//  Created by Chaitanya Agrawal on 01/10/14.
+//  Copyright (c) 2014 Chaitanya Agrawal. All rights reserved.
+//
+
+#import "RegisterController.h"
+
+@interface RegisterController ()
+
+@end
+
+@implementation RegisterController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [[UILabel appearanceWhenContainedIn:[UITextField class], nil] setTextColor:[UIColor whiteColor]];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
